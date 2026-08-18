026bde280efb47783e0c18ca70be52b4773f219c
Vinicius Chaves Alves de Almeida <valves@calf.local>
2026-08-18 11:39:15 -0300

    Primeiro commit da calculadora

---
+++ bin/calculadora.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+  String? entrada = "";
+  List<String> operacoes = <String> ["+", "-", "*", "/"];
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+  void multiplicacao() {
+    print (numeroUm * numeroDois);
+  }
+
+  void calcular(){
+    
+  switch (operacao) {
+  case "+":
+  soma();
+  break;
+
+  case "-":
+  subtracao();
+  break;
+
+  case "/":
+  divisao();
+  break;
+  
+  case "*":
+  multiplicacao();
+  break;
+  default:
+  print("Operação inválida");
+  
+  }
+}
+
+void getOperacao(){
+  print ("Digite uma operação:");
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    if (operacoes.contains(entrada)){
+      operacao = entrada!;
+    }
+
+  }
+} 
+
+  print("Digite o primeiro valor:");
+
+    entrada = stdin.readLineSync();
+    
+    if (entrada != null) {
+      if (entrada != "") {
+      numeroUm = double.parse(entrada!);
+      }     
+    }
+
+getOperacao();
+
+print ("Digite o segundo valor:");
+
+entrada = stdin.readLineSync();
+    
+    if (entrada != null) {
+      if (entrada != "") {
+      numeroDois = double.parse(entrada!);
+      }     
+    }
+
+print("O  da operação resultado é:");
+
+   calcular();
+
+}
